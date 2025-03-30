f0b40679806e979d7e189f6295886d85cdfd5fe3
vasu kamani <vasukamani.ce@gmail.com>
2025-03-31 03:59:32 +0530

    user history

--- backend/src/routes/transaction.routes.js
+++ backend/src/routes/transaction.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import isAuthenticated from "../middlewares/Authentication.js";
-import { Do_transaction, GetHistory } from "../controllers/transaction.controller.js";
+import { Do_transaction, GetHistory, userGetHistory } from "../controllers/transaction.controller.js";
 
 const router = express.Router();
 
 router.post('/', isAuthenticated, Do_transaction);
 router.get('/history' , isAuthenticated , GetHistory);
+router.get('/user/history' , isAuthenticated , userGetHistory);
 
 export default router;
